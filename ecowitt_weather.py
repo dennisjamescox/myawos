6cd38cabbd2fa671d46867a857e3b42ce25f69e1
Dennis Cox <dennisjcox@gmail.com>
2022-12-22 21:28:28 -0600

    Support for the sample file

--- ecowitt_weather.py
+++ ecowitt_weather.py
@@ -16,10 +16,17 @@ import requests
 
 headers = {'Accept' : 'application/json'}
 
+# USE ACTUAL API
 # doc.ecowitt.net/web/
-r = requests.get('https://api.ecowitt.net/api/v3/device/real_time?application_key=APPLICATION_KEY&api_key=API_KEY&mac=YOUR_MAC_CODE_OF_DEVICE&call_back=all', headers=headers)
+#r = requests.get('https://api.ecowitt.net/api/v3/device/real_time?application_key=APPLICATION_KEY&api_key=API_KEY&mac=YOUR_MAC_CODE_OF_DEVICE&call_back=all', headers=headers)
 
-results = json.loads(r)
+# USE SAMPLE FILE
+r = open('ecowitt_sample.json')
+
+results = json.load(r)
+
+# Uncomment for debugging
+# print (json.dumps(results, indent=4))
 
 zulutime = datetime.datetime.utcnow()
 
@@ -32,22 +39,31 @@ weatherFile = open('/home/pi/weather_station/weather.txt', 'w')
 print(f"{hours_minutes}", file = weatherFile)
 
 # wind direction
-wind_direction = results["wind_direction"]
+wind_direction = results['data']['wind']['wind_direction']['value']
 print(wind_direction,file = weatherFile)
 
 
 # wind speed
-print(results["wind_speed"], file = weatherFile)
+wind_speed = results['data']['wind']['wind_speed']['value']
+Fwind_speed = float(wind_speed)
+print(f"{round(Fwind_speed)}", file = weatherFile)
 
-# altimeter    
-print(results["pressure"], file = weatherFile)
+# altimeter
+altimeter = results['data']['pressure']['absolute']['value']
+print(altimeter, file = weatherFile)
 
-# outside temp    
-print(results["temperature"], file = weatherFile)
+# outside temp
+outside_temp = results['data']['outdoor']['temperature']['value']
+Foutside_temp = float(outside_temp)
+print(f"{round(Foutside_temp)}", file = weatherFile)
 
 # dewpoint
-print(results["dew_point"], file = weatherFile)
+dewpoint = results['data']['outdoor']['dew_point']['value']
+Fdewpoint = float(dewpoint)
+print(f"{round(Fdewpoint)}", file = weatherFile)
 
 
 
 weatherFile.close()
+
+r.close()
