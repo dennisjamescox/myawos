688fa21e531c2449605a95a1bcf8be7017a3f9c9
Dennis Cox <dennisjcox@gmail.com>
2022-12-23 10:00:34 -0600

    Added changes done by Johann

--- ecowitt_weather.py
+++ ecowitt_weather.py
@@ -1,4 +1,6 @@
-# ecowitt api ???? Taking a guess
+# ecowitt api
+# dennisjcox@gmail.com
+# av.wiesheu@bayern-mail.de
 # December 22 2022
 # output is weather.txt ---
 # time in zulu
@@ -18,15 +20,22 @@ headers = {'Accept' : 'application/json'}
 
 # USE ACTUAL API
 # doc.ecowitt.net/web/
-#r = requests.get('https://api.ecowitt.net/api/v3/device/real_time?application_key=APPLICATION_KEY&api_key=API_KEY&mac=YOUR_MAC_CODE_OF_DEVICE&call_back=all', headers=headers)
-
-# USE SAMPLE FILE
-r = open('ecowitt_sample.json')
-
-results = json.load(r)
+r = requests.get("https://api.ecowitt.net/api/v3/device/real_time?application_key=XXXXXXXXXX&api_key=YYYYYYYYY&mac=ZZZZZZZZ&call_back=all")
 
 # Uncomment for debugging
-# print (json.dumps(results, indent=4))
+#print("raw data from ecowitt")
+#print(r.text)
+#print("---------------------------------------------------------------")
+#print("---------------------------------------------------------------")
+
+# USE SAMPLE FILE
+#r = open('/home/pi/ecowitt/ecowitt_sample.json')
+
+results = json.loads(r.text)
+
+# Uncomment for debugging
+#print("parsed data from ecowitt:")
+#print(json.dumps(results, indent=4))
 
 zulutime = datetime.datetime.utcnow()
 
@@ -34,7 +43,8 @@ zulutime = datetime.datetime.utcnow()
 hours_minutes = zulutime.strftime("%H%M")
 
 
-weatherFile = open('/home/pi/weather_station/weather.txt', 'w')
+#weatherFile = open('/home/pi/weather_station/weather.txt', 'w')
+weatherFile = open('/home/pi/weather_station/ramdisk/weather.txt', 'w')
 
 print(f"{hours_minutes}", file = weatherFile)
 
@@ -46,20 +56,28 @@ print(wind_direction,file = weatherFile)
 # wind speed
 wind_speed = results['data']['wind']['wind_speed']['value']
 Fwind_speed = float(wind_speed)
+# converto to knots
+Fwind_speed = Fwind_speed * 0.87
 print(f"{round(Fwind_speed)}", file = weatherFile)
 
 # altimeter
 altimeter = results['data']['pressure']['absolute']['value']
-print(altimeter, file = weatherFile)
+# convert to hPa
+Faltimeter = float(altimeter) / 0.029529980164712
+print(f"{round(Faltimeter)}", file = weatherFile)
 
 # outside temp
 outside_temp = results['data']['outdoor']['temperature']['value']
 Foutside_temp = float(outside_temp)
+# convert to °C
+Foutside_temp = (Foutside_temp - 32) / 1.8
 print(f"{round(Foutside_temp)}", file = weatherFile)
 
 # dewpoint
 dewpoint = results['data']['outdoor']['dew_point']['value']
 Fdewpoint = float(dewpoint)
+# convert to °C
+Fdewpoint = (Fdewpoint - 32) / 1.8
 print(f"{round(Fdewpoint)}", file = weatherFile)
 
 
