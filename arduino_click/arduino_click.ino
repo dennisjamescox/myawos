7e5cfc593bbe1f6380d5a5a9063f56e8c980d33b
Dennis Cox <dennisjcox@gmail.com>
2022-12-23 09:16:15 -0600

    Added support for 3,5 and/or 7 clicks

--- arduino_click/arduino_click.ino
+++ arduino_click/arduino_click.ino
@@ -4,15 +4,27 @@
  * dennisjcox@gmail.com
  */
 
-const int PTTSeconds = 5000; // they have 5 seconds to get 5 clicks
+/* CAUTION: One or more of these values must be set */
+
+/* Set this value to 1 to allow for 3 click support */
+int THREECLICK = 0;
+
+/* Set this value to 1 to allow for 5 click support */
+int FIVECLICK = 1;
+
+/* Set this value to 1 to allow for 7 click support */
+int SEVENCLICK = 0;
+
+
+const int PTTSeconds = 5000;  // NOTE: Change this value to the maximum click count you want - so 7000 for 7 seconds 5000 for 5 seconds  3000 for 3 seconds
 const int PlayBackDelay = 30000; // 30 seconds between play backs
 
+int ClickSuccessFlag = 0;
 int WeatherReportCount = 0;
 int A0Value = 0;                        // value intialization
 int count = 0;
 int timedOut = 0;
 int diffTimeFlag = 0;
-
 
 int PTTClickCount = 0; // Count # of clicks
 
@@ -26,12 +38,14 @@ void setup() {
     Serial.begin(9600);                   // Communication rate
 }
 
+
+
 void loop() 
 {
 
  if(start == 0)
   {
-//    Serial.println("Click listener process started");
+ //   Serial.println("Click listener process started");
     start = 1;
   }
   
@@ -52,41 +66,41 @@ void loop()
   }
 
   // count is used to make sure the values held for at least 5 milliseconds
-    if(count > 5)
-      {
-           Serial.print("PTT Pressed ");
-	         PTTClickCount++;
-	         Serial.println(PTTClickCount);
-	         delay(40);
-	         count = 0;
-      }
+  if(count > 3)
+  {
+     Serial.print("PTT Pressed ");
+     PTTClickCount++;
+     Serial.println(PTTClickCount);
+     delay(40);
+     count = 0;
+  }
 
-  // 5 clicks plays the weather
-    if(PTTClickCount == 5)
-      {
-          Serial.println("Play Weather");
-          delay(PlayBackDelay);
+  if(THREECLICK && PTTClickCount == 3)
+  {
+    ClickSuccessFlag = 3;
+  }
+  else if(FIVECLICK && PTTClickCount == 5)
+  {
+    ClickSuccessFlag = 5;
+  }
+  else if(SEVENCLICK && PTTClickCount == 7)
+  {
+    ClickSuccessFlag = 7;
+  }
 
-    // reset variables
-        count = 0;
-        PTTClickCount = 0;
-        diffTime = 0;
-        boardTime = 0;
-        diffTimeFlag =0;
-      }
 
   if(PTTClickCount == 1 && diffTimeFlag == 0)
-    {
-        diffTime = millis();
-        diffTimeFlag = 1;
-    }
-        if(diffTime > 0)
-        {
-           boardTime = millis();
-           timedOut = boardTime - diffTime;
+  {
+     diffTime = millis();
+     diffTimeFlag = 1;
+  }
+  if(diffTime > 0)
+  {
+     boardTime = millis();
+     timedOut = boardTime - diffTime;
 
      if(timedOut > PTTSeconds)
-      {
+     {
 /*
         Serial.print("# of Clicks: ");
         Serial.print(PTTClickCount);
@@ -96,16 +110,26 @@ void loop()
         Serial.print(diffTime);
         Serial.print(" TimedOut: ");
         Serial.print(timedOut);
-        Serial.println("Timed out");
+        Serial.println(" .");
 */
+
+	if(ClickSuccessFlag > 1)
+	{
+	  Serial.print(ClickSuccessFlag);
+	  Serial.println(" Clicks");
+	  delay(PlayBackDelay);
+	}
+   
+      
         // reset variables
+	      count = 0;
         PTTClickCount = 0;
         diffTime = 0;
         boardTime = 0;
         diffTimeFlag = 0;
-          }
       }
+    }
 
   // just a delay
     delay(10);
- }
+}
