367522e2a2dc592fe25c0266c875fce20e35f7aa
buschpilot98 <121248380+buschpilot98@users.noreply.github.com>
2023-04-12 20:11:25 +0200

    clean up v2

--- v2/tools/gtts/strassham.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python3.5
-
-
-from gtts import gTTS
-import os 
-
-#os.system("cd /home/pilot/weather_station/tools/gtts; gtts-cli --lang de -f strassham.txt -o strassham.mp3")
-os.system("cd /home/pilot/weather_station/tools/gtts; play -q strassham.mp3 tempo 1.2 2>&1  >/dev/null")
-
